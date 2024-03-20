5fce2a45b22cccc6652b009b74dc876a68da0691
Matt Lindborg <ideasarethefuture@gmail.com>
2024-03-20 12:54:52 -0500

    Add project files.

---
+++ Controllers/RandomNumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomNumberAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomNumberController : ControllerBase
+    {
+        private readonly Random _rng = new();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_rng.Next(1, 101)); // Generate a random number between 1 and 100
+        }
+    }
+}
